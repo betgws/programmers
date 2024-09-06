1b1c5503dc83b1481908c643fa2688bb683fc276
dayeong <dayeongyun184@gmail.com>
2024-09-07 00:00:20 +0900

    프로그래머스 두 원소 사이 정수 쌍

--- circle.py
+++ circle.py
@@ -1,38 +1,9 @@
-import math
-def main():
-    findDotNumber(3)
+from math import sqrt
 
 def solution(r1, r2):
-    answer = 0
-    c1 = findDotNumber(r1)
-    c2 = findDotNumber(r2)
-
-    answer = c2 -c1 +4
-    return answer
-
-def findDotNumber(r):
-    x = 0
-    y = 0 
-    answer = 0
-
-    for xx in range(r+1):
-
-        rargeY = r*r - xx*xx
-        yy = math.sqrt(rargeY)
-
-        while( y <= yy):
-            answer += 1
-            y += 1
-
-        y= 0
-        print(answer)
- 
-    print(answer*4 - (r+1)*4)
-
-    return answer*4 - r*4 + 1
-
-solution(2,3)
-
-
-
-
+    quar = 0
+    for i in range(0, r1):
+        quar += int(sqrt(r2**2 - i**2)) - int(sqrt(r1**2 - i**2 - 1))
+    for i in range(r1, r2):
+        quar += int(sqrt(r2**2 - i**2))
+    return quar * 4 
