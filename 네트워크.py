95ba9f3de63cac03424b606e48d463207c3586c1
dayeong <dayeongyun184@gmail.com>
2025-02-10 00:23:18 +0900

    프로그래머스 네트워크

--- 네트워크.py
+++ 네트워크.py
@@ -1,41 +1,19 @@
-from collections import deque
-
 def solution(n, computers):
-    
-    answer = []
-    visited= [False] * (n+1)
-    inList = []
+    answer = 0
+    visited = [False] * n
+
+    def bfs(index):
+        queue = deque([index])
+        while queue:
+            v = queue.popleft()
+            for ind, connected in enumerate(computers[v]):
+                if connected == 1 and not visited[ind]:
+                    visited[ind] = True
+                    queue.append(ind)
 
     for i in range(n):
-        if(i not in inList):
-            cnt = bfs(i,computers,visited)
-            answer.append(cnt)
-            for i in cnt:
-                inList.append(i)
-
+        if not visited[i]:
+            bfs(i)
+            answer += 1
     
-    
-    return len(answer)
-
-def bfs(index, computers, visited):
-
-    queue = deque()
-    queue.append(index)
-    visited[index] = True
-    cnt = [index]
-
-    while(queue):
-        v = queue.popleft()
-        for ind ,a in enumerate(computers[v]):
-            if a == 1 and visited[ind] == False:
-                queue.append(ind)
-                cnt.append(ind)
-                visited[ind] = True
-
-    return cnt
-
-solution(3,[[1, 1, 0], [1, 1, 1], [0, 1, 1]])
-
-
-
-    
+    return answer
