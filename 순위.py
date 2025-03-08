6376d9e21aff7a6d36e0102a8286ad99edfc5419
dayeong <dayeongyun184@gmail.com>
2025-03-09 00:35:32 +0900

    순위 프로그래머스

--- 순위.py
+++ 순위.py
@@ -1,25 +1,70 @@
+from collections import deque
+
+
 def solution(n, results):
-    # 그래프 초기화 (0: 알 수 없음, 1: 이김, -1: 짐)
-    graph = [[0] * (n + 1) for _ in range(n + 1)]
-    
-    # 경기 결과 반영
-    for winner, loser in results:
-        graph[winner][loser] = 1  # 승리
-        graph[loser][winner] = -1  # 패배
 
-    # 플로이드-워셜 알고리즘 수행
-    for k in range(1, n + 1):  # 중간 선수
-        for i in range(1, n + 1):  # 출발 선수
-            for j in range(1, n + 1):  # 도착 선수
-                if graph[i][k] == 1 and graph[k][j] == 1:
-                    graph[i][j] = 1  # i가 j를 이김
-                    graph[j][i] = -1  # j는 i에게 짐
-
-    # 정확한 순위를 알 수 있는 선수 수 계산
+    winList =[[]for _ in range(n+1)]
+    loseList = [[] for _ in range(n+1)]
     answer = 0
-    for i in range(1, n + 1):
-        count = sum(1 for j in range(1, n + 1) if graph[i][j] != 0)
-        if count == n - 1:  # 다른 모든 선수와 승패가 결정됨
-            answer += 1
+
+    for i in results:
+        winList[i[0]].append(i[1])
+        loseList[i[1]].append(i[0])
+
+    
+    for i in range(n):
+
+        visited = [False]*(n)
+        visited[i] = True
+
+        winQueue = deque()
+        winQueue.append(i+1)
+
+        loseQueue = deque()
+        loseQueue.append(i+1)
+
+        c = 1
+        t = 0
+    
+
+        while(winQueue):
+            if(c == n):
+                answer = answer + 1
+                t = 1
+                break
+
+            a = winQueue.popleft()
+            
+            for i in winList[a]:
+                if(visited[i-1] == False):
+                    visited[i-1] = True
+                    c = c+1
+                    winQueue.append(i)
+        
+
+        while(loseQueue and t == 0):
+
+            if(c == n):
+                answer = answer + 1
+                t = 1
+                break
+
+            a = loseQueue.popleft()
+            
+            for i in loseList[a]:
+                if(visited[i-1] == False):
+                    visited[i-1] = True
+                    c = c+1
+                    loseQueue.append(i)
+
+        if(t == 0 and c == n):
+            answer = answer +1
+        
+
 
     return answer
+
+
+
+
+solution(5,[[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]])
