046336ccaff71fcf4bb3e09a93cef589364aa110
betgws <dayeongyun184@gmail.com>
2025-06-27 20:08:11 +0900

    프로그래머스 이모티콘 할인 행사

--- 택배배달과수거하기.py
+++ 택배배달과수거하기.py
@@ -4,44 +4,67 @@ def solution(cap, n, deliveries, pickups):
     deliver = n - 1
     get = n - 1
 
-    # 첫 시작점 찾기
-    while deliveries[deliver] == 0:
-        deliver = deliver - 1
-    while pickups[get] == 0:
-        get = get - 1 
+    while deliver >= 0 or get >= 0:
+        # deliver/get 포인터가 가리키는 유효한 위치 찾기
+        while deliver >= 0 and deliveries[deliver] == 0:
+            deliver -= 1
+        while get >= 0 and pickups[get] == 0:
+            get -= 1
 
-    total = sum(deliveries) + sum(pickups)
-    
-    while total:
-        answer = answer + (max(deliver,get) + 1)*2
+        # 종료 조건
+        if deliver < 0 and get < 0:
+            break
+
+        # 현재 라운드 최대 거리
+        distance = max(deliver, get) + 1
+        answer += distance * 2
 
         deliverCap = cap
         getCap = cap
 
-        while(deliver >= 0 and deliveries[deliver] <= deliverCap):
-            
-            total = total - deliveries[deliver]
-            deliverCap = deliverCap - deliveries[deliver]
-            deliveries[deliver] = 0
-            deliver = deliver - 1 
+        # 배달 처리
+        while deliver >= 0 and deliverCap > 0:
+            if deliveries[deliver] <= deliverCap:
+                deliverCap -= deliveries[deliver]
+                deliveries[deliver] = 0
+                deliver -= 1
+            else:
+                deliveries[deliver] -= deliverCap
+                break
 
-        while(get >= 0 and pickups[get] <= getCap):
-            total = total - pickups[get]
-            getCap = getCap - pickups[get]
-            pickups[get] = 0
-            get = get - 1
+        # 수거 처리
+        while get >= 0 and getCap > 0:
+            if pickups[get] <= getCap:
+                getCap -= pickups[get]
+                pickups[get] = 0
+                get -= 1
+            else:
+                pickups[get] -= getCap
+                break
 
-        if(deliver > 0 and deliveries[deliver] > deliverCap):
-            deliveries[deliver] = deliveries[deliver] - deliverCap
-            total = total - deliverCap
-            deliverCap = 0
-
-        if(get> 0 and pickups[get] > getCap):
-            pickups[get] = pickups[get] - getCap
-            total = total - getCap
-            getCap = 0
-        
-        
     return answer
 
-solution(2,7,[1, 0, 2, 0, 1, 0, 2],[0, 2, 0, 1, 0, 2, 0])
+def solution2(cap, n, deliveries, pickups):
+    answer = 0
+    delivery = 0
+    pickup = 0
+    
+    for i in range(n-1, -1, -1):  # ▶️ 가장 먼 집부터 (역순)
+        delivery += deliveries[i]
+        pickup += pickups[i]
+
+        while delivery > 0 or pickup > 0:  # ▶️ 한 번 더 가야 하는 경우
+            delivery -= cap
+            pickup -= cap
+            answer += (i + 1) * 2  # ▶️ 왕복 거리 추가
+    return answer
+        
+        
+
+
+        
+
+
+
+
+solution1(2,7,[1, 0, 2, 0, 1, 0, 2],[0, 2, 0, 1, 0, 2, 0])
