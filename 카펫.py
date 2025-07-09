6984b4fe27081c1ef8b214e56318b9bbb24b99e2
betgws <dayeongyun184@gmail.com>
2025-07-09 11:42:15 +0900

    프로그래머스 카펫 2

--- 카펫.py
+++ 카펫.py
@@ -1,23 +1,30 @@
-def solution(brown, yellow):
 
+
+
+
+
+def solution(brown,yellow):
     answer = []
-    yellowMList = []
-    canBrownList = []
 
-    for i in range(yellow):
-        if(yellow % (i+1) == 0):
-            yellowMList.append((i+1, yellow//(i+1)))
+    for vertical in range((yellow//2)):
+        vertical = vertical + 1
+        if(yellow%vertical != 0):
+            continue
+        horizontal = int(yellow/vertical)
 
-    sortedYList = [tuple(sorted(t,reverse=True)) for t in yellowMList]
-    sortedYList = set(sortedYList)
+        if(vertical*2 + horizontal*2 + 4 == brown):
+            return [horizontal+2,vertical+2]
+        
 
-    for i in sortedYList:
-        if(i[0]*2 + i[1]*2+4 == brown):
-            canBrownList.append(i)
-            break
+    
+solution(8,1)
 
-    answer.append(canBrownList[0][0]+2)
-    answer.append(canBrownList[0][1]+2)
-    return answer
 
-solution(24,24)  
+
+
+
+
+
+
+
+
