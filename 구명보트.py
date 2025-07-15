47e098fdd6d8747648970bfa4053655ee2c88178
betgws <dayeongyun184@gmail.com>
2025-07-15 15:34:39 +0900

    구명보트 greedy 프로그래머스 효율적으로 풀기

--- 구명보트.py
+++ 구명보트.py
@@ -1,32 +1,28 @@
 from collections import deque
 
 def solution(people, limit):
+    
+    answer = 0 
 
-    people.sort(reverse = True)
+    people.sort()
     people = deque(people)
-    answer = 0
-    i = 0
-    while(people):
-        current = people.popleft()
 
-        while(current <= limit):
-            
-            if(people):
-                a = people[-1]
-            else:
-                answer = answer + 1
-                break
-            if(a+current > limit):
-                answer = answer+1
-                break
-            else:
-                people.pop()
-                answer = answer +1
-                break
-    return answer
+    while people:
+        big = people.pop()
+
+        if(people and big + people[0] <= limit):
+            people.popleft() 
+
+        answer += 1
 
 
-solution([10,30,40,50,50],50)
+    return answer 
+
+             
+
+
+    
+solution([10, 10, 10, 40],45)
 
 
 
